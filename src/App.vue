2398bee8c79827694cfe30a56fc2afd4d871fc47
Virgiliu <plesca.virgiliu@gmail.com>
2024-12-05 23:30:18 +0200

    add set color and turn on turn off

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,77 @@
 <template>
   <div class="app">
-    <n-card title="Card">
-      Card Content
+    <n-card title="Control Panel">
+      <div class="panel-body">
+        <n-button @click="onTurnOn">
+          Turn On
+        </n-button>
+        <n-button @click="onTurnOff">
+          Turn Off
+        </n-button>
+        <n-color-picker :onUpdate:value="onUpdateColor" />
+        <n-button @click="setLampColor">Set lamp color</n-button>
+      </div>
     </n-card>
   </div>
 </template>
 
 <script setup lang="ts">
 import {LampService} from "@/services/lampService.ts";
+import {ref} from "vue";
+
+const isLampOn = ref(false);
+const lampColor = ref('');
+
+const toggleLamp = () => {
+  isLampOn.value = !isLampOn.value;
+  handleToggleLampCb()
+}
+
+const handleToggleLampCb = () => {
+  if (isLampOn.value) {
+    onTurnOn()
+  }
+  else {
+    onTurnOff()
+  }
+}
+
+const onUpdateColor = (value: string) => {
+  lampColor.value = value
+  LampService.setLampColor(lampColor.value.slice(5, -1).replace(/\s+/g, ""))
+}
+
+const setLampColor = () => {
+  LampService.setLampColor(lampColor.value.slice(5, -1).replace(/\s+/g, ""))
+}
 
 const onTurnOn = () => {
-  LampService.turnOnLamp()
-      .then((res) => {
-        console.log(res.data);
-      })
+  LampService.turnOnLamp();
 }
 
 const onTurnOff = () => {
-  LampService.turnOffLamp()
+  LampService.turnOffLamp();
 }
 
 </script>
 
 <style scoped>
   .app {
+    padding: 10% 20%;
+  }
+
+  .panel-body {
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
-    width: 100%;
-    height: 100vh;
-    background: #9f9f9f;
+    width: 300px;
+
+    & > * {
+      width: 100%;
+    }
+
+    .turnonbutton {
+      width: 50%;
+    }
   }
 </style>
--- src/services/lampService.ts
+++ src/services/lampService.ts
@@ -2,12 +2,22 @@ import axios from "axios";
 
 const API_URL = "http://192.168.4.1:80/";
 
+const getUrl = (endpoint: string) => `${API_URL}${endpoint}`;
+
+const turnOnEndpoint = "turnOn"
+const turnOffEndpoint = "turnOff"
+const setColorEndpoint = "setColor"
+
 export const LampService = {
     turnOnLamp() {
-        return axios.get(API_URL);
+        return axios.get(getUrl(turnOnEndpoint));
     },
 
     turnOffLamp() {
-        return axios.get(API_URL, { params: { lampState: 0 }});
+        return axios.get(getUrl(turnOffEndpoint));
+    },
+
+    setLampColor(color: string) {
+        return axios.get(getUrl(setColorEndpoint), { params: { color: color } })
     },
 }
